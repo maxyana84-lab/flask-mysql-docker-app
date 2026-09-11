5b4e6cca962cd85bf9c98ce547457a56f622a18f
Maxim Shefter <maxyana84@gmail.com>
2026-09-11 21:52:22 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,5 +1,7 @@
-USE your_db_name;
-
 CREATE TABLE IF NOT EXISTS page_counter (
 	count INT NOT NULL DEFAULT 0
-);	
+);
+
+INSERT INTO page_counter (count)
+SELECT 0
+WHERE NOT EXISTS (SELECT 1 FROM page_counter);
